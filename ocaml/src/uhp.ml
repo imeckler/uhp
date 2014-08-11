ea642906a0212234bdba7b0d1744e11c361fc57f
Izaak Meckler <ihmeckler@gmail.com>
2014-08-11 12:39:50 -0500

    it's a game now

--- ocaml/src/uhp.ml
+++ ocaml/src/uhp.ml
@@ -2,13 +2,13 @@ open Core
 
 type pos = float Point.t
 
-type state = { pos : pos ; dir : Angle.t }
-(* Forward/Backward corresponds to increasing/decreasing angle for
- * Semi_circle and increasing/decreasing y-coordinate for Vertical *)
-type orientation = Forward | Backward
-type geodesic =
-  | Semi_circle of float * float
-  | Vertical    of float
+type player_state =
+  { pos : pos
+  ; dir : Angle.t 
+  ; radius : float
+  }
+
+type state = { player : player_state; rival : player_state }
 
 let epsilon         = 1e-11
 let scale_factor    = 0.05
@@ -22,27 +22,60 @@ let arrows = Frp.project_with Jq.arrows (Frp.Stream.ticks 30.) ~f:(fun (ax, ay) 
 let movement_vect {pos = (_, y); dir} =
   Vector.scale (scale_factor *. y) (Angle.cos dir, Angle.sin dir)
 
-let state =
-  Frp.scan arrows ~init:{pos = (100., 100.); dir = Angle.of_degrees 0.}
-    ~f:(fun ({pos; dir} as s) (ax, ay) -> {
+let player_state =
+  Frp.scan arrows ~init:{pos = (100., 100.); dir = Angle.of_degrees 0.; radius = 1.}
+    ~f:(fun ({pos; dir} as s) (ax, ay) -> { s with
         dir = Angle.(dir + angle_scale * ax);
         pos = Vector.add pos (Vector.scale ay (movement_vect s))
     })
 
-(*
-let geodesic_through =
-  let on_vertical x = cos x = 0. in
-  fun {pos; dir} ->
-    let dir_rad = Angle.to_radians dir in
-    if on_vertical dir_rad
-    then (Vertical (snd pos), if sin dir_rad > 0. then Forward else Backward)
-    else Semi_circle
+let visual_radius {radius; pos=(_, y)} = 0.15 *. y *. radius
 
-*)
-let player = let open Draw in let open Frp.Behavior in
-  Draw.circle ~fill:(return Color.black)
-    (map state ~f:(fun {pos=(_,y)} -> 0.15 *. y))
+let random_state () =
+  { pos = Random.float width', Random.float height'
+  ; dir = Angle.of_degrees (Random.float 360.)
+  ; radius = 1.
+  }
+
+let rival_state, collisions = let open Frp in
+  let margin = 10. in
+  let forced_turn =
+    let detect d t1 t2 v =
+      if v < margin then Some t1 else if v > d -. margin then Some t2 else None
+    in
+    fun (x, y) ->
+      Option.map ~f:Angle.of_degrees (match detect width' 0. 180. x, detect height' 90. 270. y with
+        | axo, None -> axo
+        | None, ayo -> ayo
+        | Some ax, Some ay -> Some (ax +. ay /. 2.))
+  in
+  let is_collision p r = Vector.(norm (p.pos - r.pos)) < visual_radius p +. visual_radius r in
+  let both = 
+    scan (Stream.deltas 30.) ~init:(random_state (), 0) ~f:(fun (s, coll_count) u ->
+      let collision = is_collision (Behavior.peek player_state) s in
+      if collision then (random_state (), coll_count + 1)
+      else
+        let s' = { s with
+            dir = (match forced_turn s.pos with
+              | None -> Angle.(s.dir + Angle.of_degrees (Random.float 60. -. 30.))
+              | Some d -> d);
+            pos = Vector.add s.pos (movement_vect s)
+          }
+        in
+        (s', coll_count))
+  in
+  Behavior.map ~f:fst both, Behavior.map ~f:snd both
+
+let counter = Draw.text (Frp.Behavior.map ~f:string_of_int collisions)
+  (Frp.Behavior.return (0., 0.))
+
+let draw_agent color state = let open Draw in let open Frp.Behavior in
+  Draw.circle ~fill:(return color)
+    (map state ~f:visual_radius)
     (map state ~f:(fun {pos} -> pos))
+
+let player = draw_agent Color.black player_state
+let rival  = draw_agent Color.red rival_state
 
 let standard_arrow = let open Draw in let open Frp.Behavior in
   let x_offset = 2. /. 3. in
@@ -67,7 +100,7 @@ let draw_arrow src_dst =
 let dir_arrow : Draw.t = let open Draw in let open Frp.Behavior in
   let d = 1. in
   transform standard_arrow
-    Transform.(map state ~f:(fun {pos; dir} -> let y = snd pos in
+    Transform.(map player_state ~f:(fun {pos; dir} -> let y = snd pos in
       let sine = Angle.sin dir in
       let sf =
         if abs_float sine < epsilon then d *. y
@@ -90,7 +123,7 @@ let checked =
 let drawing = let open Draw in
   let flip = [|Transform.rotate (Angle.of_degrees 180.) ~about:(width' /. 2., height' /. 2.)|] in
   let d =
-    transform (pictures [|dir_arrow; player|])
+    transform (pictures [|counter; dir_arrow; player; rival|])
       (Frp.Behavior.map flipped ~f:(fun is_flipped ->
         if is_flipped
         then flip
